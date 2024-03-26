3a5780443effd0cc9d64947c53f0899e8d211248
imperialhound <david-dppedersen@proton.me>
2024-03-26 18:58:23 +0100

    init: setup base fuzzy-finder repo

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/imperialhound/fuzzy-clone
+
+go 1.21.0
---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+func main() {
+  // TODO(dpe): Get repo configuration file or environment variable
+  // Configs:
+  // token 
+  // public/private endpoints
+  // preferences
+  // paths
+
+  // TODO(dpe): Run root command
+
+    // TODO(dpe): github fuzzy finder
+
+    // TODO(dpe): gitlab fuzz finder
+}
